0d873788e8bedecff2ec56ff8c597874bf710911
ayush4484 <ayushraj4484@gmail.com>
2025-03-07 11:36:18 +0530

    i am learning to create local or global variable

---
+++ variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int glo = 5;
+void sum()
+{
+    int a ;
+    cout<<glo<<endl;
+
+}
+    int main()
+{
+    int glo = 3;
+    glo = 56;
+    sum();
+    cout<< glo;
+    return 0;
+}
